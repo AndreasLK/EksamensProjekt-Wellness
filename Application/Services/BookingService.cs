a570c3a16f343025bfe933cfe003b4fca4947d5b
AndreasLK <Andreaslkrus@gmail.com>
2026-04-19 10:48:31 +0200

    Added constructor to bookingservice

--- Application/Services/BookingService.cs
+++ Application/Services/BookingService.cs
@@ -1,12 +1,37 @@
-﻿using Domain.Interfaces;
+﻿using Domain.Entities;
+using Domain.Entities.Clinics;
+using Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Application.Services
 {
-    public class BookingService //: IBookingService
+    public class BookingService : IBookingService
     {
-        //TODO
+        private readonly IRepository<Booking> _bookingRepository;
+        private readonly IRepository<Practitioner> _practitionerRepository;
+        private readonly IRepository<Clinic> _clinicRepository;
+        private readonly ITravelTimeService _travelService;
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+
+        public BookingService(
+            IRepository<Booking> bookingRepository,
+            IRepository<Practitioner> practitionerRepository,
+            IRepository<Clinic> clinicRepository,
+            ITravelTimeService travelService,
+            IDateTimeProvider dateTimeProvider)
+        {
+            this._bookingRepository = bookingRepository;
+            this._practitionerRepository = practitionerRepository;
+            this._clinicRepository = clinicRepository;
+            this._travelService = travelService;
+            this._dateTimeProvider = dateTimeProvider;
+        }
+
+
+        
+
     }
 }
