b1b83799a4d867e3af1a3055f8a5b497f2219f7c
AndreasLK <Andreaslkrus@gmail.com>
2026-04-22 13:53:29 +0200

    Added RoomSelectDTto for when a receptionist need to select room for a booking

---
+++ UseCase/Rooms/Queries/RoomSelectDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseCase.Rooms.Queries
+{
+    /// <summary>
+    /// Represents a treatment room for selection within the calendar or booking forms.
+    /// </summary>
+    /// <param name="Id">Unique identifier for the room.</param>
+    /// <param name="Name">The room name or number (like "Room 101").</param>
+    /// <param name="ClinicId">The ID of the parent clinic this room belongs to.</param>
+    public record RoomSelectDto(Guid Id, string Name, Guid ClinicId);
+}
