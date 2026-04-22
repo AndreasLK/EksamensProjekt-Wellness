94a6e98e2af4e211f0a220b9b8d4c85c26266fed
AndreasLK <Andreaslkrus@gmail.com>
2026-04-22 10:16:04 +0200

    Added Selection for Practitioner DTO

---
+++ UseCase/Practitioners/Queries/PractitionerSelectDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseCase.Practitioners.Queries
+{
+    /// <summary>
+    /// Represents a practitioner for selection in the booking interface.
+    /// </summary>
+    /// <param name="Id">Unique identifier.</param>
+    /// <param name="FullName">The combined first and last name for display.</param>
+    /// <param name="Initials">Shortened name for small calendar cards.</param>
+    public record PractitionerSelectDto(
+        Guid Id,
+        string FullName,
+        string Initials);
+}
